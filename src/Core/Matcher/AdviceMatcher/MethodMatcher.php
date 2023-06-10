90aaaa11d8905b4812cb1a204fca7e39feb8d561
WalterWoshid <wotschel.valentin@googlemail.com>
2023-06-10 17:53:55 +0200

    Refactored MethodMatcher

--- src/Core/Matcher/AdviceMatcher/MethodMatcher.php
+++ src/Core/Matcher/AdviceMatcher/MethodMatcher.php
@@ -3,7 +3,8 @@
 namespace Okapi\Aop\Core\Matcher\AdviceMatcher;
 
 use Okapi\Aop\Core\Container\AdviceType\MethodAdviceContainer;
-use Roave\BetterReflection\Reflection\ReflectionClass;
+use Roave\BetterReflection\Reflection\ReflectionClass as BetterReflectionClass;
+use Roave\BetterReflection\Reflection\ReflectionMethod as BetterReflectionMethod;
 
 /**
  * # Method Matcher
@@ -17,46 +18,111 @@ class MethodMatcher
      * Match the given method advice container for the given class.
      *
      * @param MethodAdviceContainer $methodAdviceContainer
-     * @param ReflectionClass       $refClassToMatch
+     * @param BetterReflectionClass $refClassToMatch
      *
      * @return MethodAdviceContainer|null
      */
     public function match(
         MethodAdviceContainer $methodAdviceContainer,
-        ReflectionClass       $refClassToMatch,
+        BetterReflectionClass $refClassToMatch,
     ): ?MethodAdviceContainer {
-        $adviceAttributeInstance  =
-            $methodAdviceContainer->adviceAttributeInstance;
         $newMethodAdviceContainer = null;
 
         foreach ($refClassToMatch->getMethods() as $refMethodToMatch) {
-            // Check for explicit match
+            // Match explicit aspects
             if ($methodAdviceContainer->isExplicit()) {
-                // Only add methods that have the attribute of the aspect
-                $aspectClassName = $methodAdviceContainer->aspectClassName;
-                foreach ($refMethodToMatch->getAttributes() as $refAttribute) {
-                    if ($refAttribute->getName() === $aspectClassName) {
-                        if (!$newMethodAdviceContainer) {
-                            $newMethodAdviceContainer = clone $methodAdviceContainer;
-                        }
-
-                        $newMethodAdviceContainer->addMatchedMethod($refMethodToMatch);
-                    }
-                }
+                $newMethodAdviceContainer = $this->matchExplicit(
+                    $methodAdviceContainer,
+                    $refMethodToMatch,
+                    $newMethodAdviceContainer,
+                );
             } else {
-                // Check for implicit match
-                $methodNameToMatch = $refMethodToMatch->getName();
-
-                if ($adviceAttributeInstance->method->matches($methodNameToMatch)) {
-                    if (!$newMethodAdviceContainer) {
-                        $newMethodAdviceContainer = clone $methodAdviceContainer;
-                    }
-
-                    $newMethodAdviceContainer->addMatchedMethod($refMethodToMatch);
-                }
+                // Match implicit aspects
+                $newMethodAdviceContainer = $this->matchImplicit(
+                    $methodAdviceContainer,
+                    $refMethodToMatch,
+                    $newMethodAdviceContainer,
+                );
             }
         }
 
         return $newMethodAdviceContainer;
+    }
+
+    /**
+     * Match explicit aspects.
+     *
+     * @param MethodAdviceContainer      $methodAdviceContainer
+     * @param BetterReflectionMethod     $refMethodToMatch
+     * @param MethodAdviceContainer|null $newMethodAdviceContainer
+     *
+     * @return MethodAdviceContainer|null
+     */
+    protected function matchExplicit(
+        MethodAdviceContainer $methodAdviceContainer,
+        BetterReflectionMethod $refMethodToMatch,
+        ?MethodAdviceContainer $newMethodAdviceContainer,
+    ): ?MethodAdviceContainer {
+        $aspectClassName = $methodAdviceContainer->aspectClassName;
+
+        // Only add methods that have the attribute of the aspect
+        foreach ($refMethodToMatch->getAttributes() as $refAttribute) {
+            if ($refAttribute->getName() === $aspectClassName) {
+                $newMethodAdviceContainer = $this->createNewMethodAdviceContainer(
+                    $methodAdviceContainer,
+                    $newMethodAdviceContainer,
+                );
+
+                $newMethodAdviceContainer->addMatchedMethod($refMethodToMatch);
+            }
+        }
+
+        return $newMethodAdviceContainer;
+    }
+
+    /**
+     * Match implicit aspects.
+     *
+     * @param MethodAdviceContainer      $methodAdviceContainer
+     * @param BetterReflectionMethod     $refMethodToMatch
+     * @param MethodAdviceContainer|null $newMethodAdviceContainer
+     *
+     * @return MethodAdviceContainer|null
+     */
+    protected function matchImplicit(
+        MethodAdviceContainer $methodAdviceContainer,
+        BetterReflectionMethod $refMethodToMatch,
+        ?MethodAdviceContainer $newMethodAdviceContainer,
+    ): ?MethodAdviceContainer {
+        $methodNameToMatch = $refMethodToMatch->getName();
+
+        if ($methodAdviceContainer->adviceAttributeInstance->method->matches($methodNameToMatch)) {
+            $newMethodAdviceContainer = $this->createNewMethodAdviceContainer(
+                $methodAdviceContainer,
+                $newMethodAdviceContainer,
+            );
+
+            $newMethodAdviceContainer->addMatchedMethod($refMethodToMatch);
+        }
+
+        return $newMethodAdviceContainer;
+    }
+
+    /**
+     * Create a new method advice container or return the given one.
+     *
+     * This method clones the existing method advice container to keep
+     * track of the matched methods.
+     *
+     * @param MethodAdviceContainer      $methodAdviceContainer
+     * @param MethodAdviceContainer|null $newMethodAdviceContainer
+     *
+     * @return MethodAdviceContainer
+     */
+    protected function createNewMethodAdviceContainer(
+        MethodAdviceContainer $methodAdviceContainer,
+        ?MethodAdviceContainer $newMethodAdviceContainer,
+    ): MethodAdviceContainer {
+        return $newMethodAdviceContainer ?? clone $methodAdviceContainer;
     }
 }
